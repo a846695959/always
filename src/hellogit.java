5f1f42e23f434f230db0f5380a9d93337d228c86
“lijie” <846695959@qq.comgit config --global jiege.846695959@qq.com 846695959@qq.com>
2021-01-07 17:06:35 +0800

    添加test工程到git

---
+++ src/hellogit.java
@@ -0,0 +1,11 @@
+public class hellogit {
+    public static void main(String[] args) {
+        System.out.println("hellogit!");
+
+        System.out.println("hellogit!");
+        System.out.println("hellogit!");System.out.println("hellogit!");System.out.println("hellogit!");System.out.println("hellogit!");
+
+
+
+    }
+}
